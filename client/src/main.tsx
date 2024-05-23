588f849cc8aeb47bf6d40df304ca66c169502d93
Xeu <thankrain@qq.com>
2024-05-23 18:38:08 +0800

    feat: add remixicon support

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom/client'
 import { App as Server } from 'rin-server/src/server'
 import App from './App'
 import './index.css'
+import 'remixicon/fonts/remixicon.css'
 const endpoint = process.env.API_URL || 'http://localhost:3001'
 export const client = treaty<Server>(endpoint)
 ReactDOM.createRoot(document.getElementById('root')!).render(
