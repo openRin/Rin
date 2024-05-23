872329bb35000b11e3dbd599ec19a4c8e82cc1d0
Xeu <thankrain@qq.com>
2024-05-23 19:10:37 +0800

    feat: add login button

--- client/src/components/header.tsx
+++ client/src/components/header.tsx
@@ -2,24 +2,31 @@ export function Header() {
     return (
         <>
             <div className="fixed">
-                <div className="relative w-screen ">
-                    <div className="flex flex-row items-center mx-32 mt-8">
-                        <img src={process.env.AVATAR} alt="Avatar" className="w-12 h-12 rounded-2xl border-2" />
-                        <div className="flex flex-col justify-center items-start mx-4">
-                            <p className="text-xl font-bold">
-                                {process.env.NAME}
-                            </p>
-                            <p className="text-sm text-neutral-500">
-                                {process.env.DESCRIPTION}
-                            </p>
+                <div className="w-screen px-32 mt-8">
+                    <div className="w-full flex justify-between items-center">
+                        <div className="mr-auto flex flex-row items-center">
+                            <img src={process.env.AVATAR} alt="Avatar" className="w-12 h-12 rounded-2xl border-2" />
+                            <div className="flex flex-col justify-center items-start mx-4">
+                                <p className="text-xl font-bold">
+                                    {process.env.NAME}
+                                </p>
+                                <p className="text-sm text-neutral-500">
+                                    {process.env.DESCRIPTION}
+                                </p>
+                            </div>
                         </div>
-                    </div>
-                    <div className="absolute left-0 right-0 top-0 bottom-0 flex flex-row justify-center items-center">
-                        <div className="flex flex-row bg-white rounded-full px-2 shadow-xl shadow-neutral-200">
-                            <NavItem title="文章" selected={true} onClick={() => { }} />
-                            <NavItem title="标签" selected={false} onClick={() => { }} />
-                            <NavItem title="朋友们" selected={false} onClick={() => { }} />
-                            <NavItem title="关于" selected={false} onClick={() => { }} />
+                        <div className="absolute left-1/2 translate-x-[-50%] flex-row justify-center items-center">
+                            <div className="flex flex-row bg-white rounded-full px-2 shadow-xl shadow-neutral-200">
+                                <NavItem title="文章" selected={true} onClick={() => { }} />
+                                <NavItem title="标签" selected={false} onClick={() => { }} />
+                                <NavItem title="朋友们" selected={false} onClick={() => { }} />
+                                <NavItem title="关于" selected={false} onClick={() => { }} />
+                            </div>
+                        </div>
+                        <div className="ml-auto flex flex-row justify-end">
+                            <div className="hover:bg-neutral-200 flex rounded-full border px-3 bg-white aspect-[1] items-center justify-center">
+                                <i className="ri-login-circle-line ri-xl"></i>
+                            </div>
                         </div>
                     </div>
                 </div>
