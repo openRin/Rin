c43ffc6f683972c9a0aee38d6ed5a3bf6099f8a0
Xeu <thankrain@qq.com>
2024-05-25 21:21:29 +0800

    feat: add remote oauth support

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -6,6 +6,7 @@ import { App as Server } from 'rin-server/src/server'
 import App from './App'
 import './index.css'
 export const endpoint = process.env.API_URL || 'http://localhost:3001'
+export const oauth_url = process.env.OAUTH_URL || (process.env.API_URL + '/user/github')
 export const client = treaty<Server>(endpoint)
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
