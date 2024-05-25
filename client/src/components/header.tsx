7a9613f403ae183d79346ef00b79071c75f05813
Xeu <thankrain@qq.com>
2024-05-26 00:39:15 +0800

    style: adjust header size on mobile device

--- client/src/components/header.tsx
+++ client/src/components/header.tsx
@@ -29,9 +29,9 @@ export function Header() {
                             <div className="w-full sm:w-max transition-all duration-500 sm:absolute sm:left-1/2 sm:translate-x-[-50%] flex-row justify-center items-center">
                                 <div className="flex flex-row items-center bg-white rounded-full px-2 shadow-xl shadow-neutral-200/30">
                                     <div className="visible opacity-100 sm:hidden sm:opacity-0 duration-300 mr-auto flex flex-row items-center">
-                                        <img src={process.env.AVATAR} alt="Avatar" className="w-12 h-12 rounded-full border-2" />
+                                        <img src={process.env.AVATAR} alt="Avatar" className="w-8 h-8 rounded-full border-2" />
                                         <div className="flex flex-col justify-center items-start mx-2">
-                                            <p className="text-base font-bold">
+                                            <p className="text-sm font-bold">
                                                 {process.env.NAME}
                                             </p>
                                             <p className="text-xs text-neutral-500">
@@ -44,7 +44,7 @@ export function Header() {
                                     {profile?.permission && <NavItem title="写作" selected={location.startsWith("/writing")} onClick={() => { setLocation("/writing") }} />}
                                     <NavItem title="朋友们" selected={location === "/friends"} onClick={() => { setLocation("/friends") }} />
                                     <NavItem title="关于" selected={location === "/about"} onClick={() => { setLocation("/about") }} />
-                                    <UserAvatar className="visible opacity-100 sm:hidden sm:opacity-0 duration-300 justify-center items-center w-12 h-12" profile={profile} />
+                                    <UserAvatar className="visible opacity-100 sm:hidden sm:opacity-0 duration-300 justify-center items-center w-8 h-8" profile={profile} />
                                 </div>
                             </div>
                             <UserAvatar className="ml-auto hidden opacity-0 sm:block sm:opacity-100 duration-300" profile={profile} />
@@ -59,7 +59,7 @@ export function Header() {
 
 function NavItem({ title, selected, onClick }: { title: string, selected: boolean, onClick: () => void }) {
     return (
-        <div onClick={onClick} className={"cursor-pointer hover:text-theme duration-300 p-4 text-base " + (selected ? "text-theme" : "")} >
+        <div onClick={onClick} className={"cursor-pointer hover:text-theme duration-300 px-2 py-4 sm:p-4 text-sm " + (selected ? "text-theme" : "")} >
             {title}
         </div >
     )
@@ -78,7 +78,7 @@ function UserAvatar({ profile, className }: { className?: string, profile?: Prof
                 </div>
             </div>
         </> : <>
-            <Icon name="ri-login-circle-line ri-xl" onClick={() => window.location.href = `${oauth_url}`} />
+            <Icon name="ri-login-circle-line ri-md" onClick={() => window.location.href = `${oauth_url}`} />
         </>}
     </div>)
 }
