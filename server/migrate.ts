5a9269c2d653f6ef9bacbb9788fa83a4c807377a
Xeu <thankrain@qq.com>
2024-05-25 23:03:17 +0800

    style: change let to const for migrate.ts & db.ts

--- server/migrate.ts
+++ server/migrate.ts
@@ -3,11 +3,11 @@ import { drizzle } from "drizzle-orm/bun-sqlite";
 import { migrate } from 'drizzle-orm/bun-sqlite/migrator';
 
 const start = new Date().getTime();
-let db_path = process.env.DB_PATH ?? "sqlite.db"
-let connection = new Database(db_path, { create: true });
-let db = drizzle(connection);
-await migrate(db, { migrationsFolder: './drizzle' });
-await connection.close();
+const db_path = process.env.DB_PATH ?? "sqlite.db"
+const connection = new Database(db_path, { create: true });
+const db = drizzle(connection);
+migrate(db, { migrationsFolder: './drizzle' });
+connection.close();
 
 const end = new Date().getTime();
 console.info(`Migration to ${db_path} took ${end - start}ms`);
--- server/src/db/db.ts
+++ server/src/db/db.ts
@@ -1,16 +1,16 @@
 import { Database } from "bun:sqlite";
 import { drizzle } from "drizzle-orm/bun-sqlite";
 import * as schema from './schema';
-let db_path = process.env.DB_PATH ?? 'sqlite.db';
+const db_path = process.env.DB_PATH ?? 'sqlite.db';
 import {} from '../../migrate'
 
-let is_exists = await Bun.file(db_path).exists()
+const is_exists = await Bun.file(db_path).exists()
 if (!is_exists) {
     console.log('Database not found, creating a new one...')
     const init_db = Bun.file("init.db");
     await Bun.write(db_path, init_db);
 }
-let connection = new Database(db_path);
-export let db = drizzle(connection, { schema: schema });
+const connection = new Database(db_path);
+export const db = drizzle(connection, { schema: schema });
 
 export default db;
