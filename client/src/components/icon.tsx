3995cda36ed49bc3739c46bc3905d80b0f19c98e
Xeu <thankrain@qq.com>
2024-05-26 22:00:09 +0800

    refactor: Add IconSmall component and implement comment functionality in feed page

--- client/src/components/icon.tsx
+++ client/src/components/icon.tsx
@@ -5,3 +5,11 @@ export function Icon({ name, className, onClick, hover = true }: { name: string,
         </div>
     )
 }
+
+export function IconSmall({ name, className, onClick, hover = true }: { name: string, className?: string, onClick: () => any, hover?: boolean }) {
+    return (
+        <div onClick={onClick} className={`max-w-8 flex rounded-full border px-2 bg-white aspect-[1] items-center justify-center ${hover ? "hover:bg-neutral-200" : ""} ` + className}>
+            <i className={name}></i>
+        </div>
+    )
+}
