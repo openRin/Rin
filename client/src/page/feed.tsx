9db4d22aa41992ec4abe2e3d0fc2cd744906168b
Xeu <thankrain@qq.com>
2024-05-26 22:07:02 +0800

    refactor: Improve error handling in Comments component and localize error messages

--- client/src/page/feed.tsx
+++ client/src/page/feed.tsx
@@ -170,12 +170,17 @@ function Comments({ id }: { id: string }) {
     const [comments, setComments] = useState<Comment[]>([])
     const [error, setError] = useState<string>()
     const ref = useRef("")
+    function errorHumanize(error: string) {
+        if (error === 'Unauthorized') return '请先登录'
+        else if (error === 'Content is required') return '评论内容不能为空'
+        return error
+    }
     function loadComments() {
         client.feed.comment({ feed: id }).get({
             headers: headersWithAuth()
         }).then(({ data, error }) => {
             if (error) {
-                setError(error.value as string)
+                setError(errorHumanize(error.value as string))
             } else if (data && Array.isArray(data)) {
                 setComments(data)
             }
