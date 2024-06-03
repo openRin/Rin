185cf2fbf2ff642600f3c293b5f719155852e2db
Xeu <thankrain@qq.com>
2024-06-03 20:06:53 +0800

    refactor: Update header component to improve user avatar display

--- client/src/components/header.tsx
+++ client/src/components/header.tsx
@@ -29,7 +29,7 @@ export function Header() {
                             <div className="w-full sm:w-max transition-all duration-500 sm:absolute sm:left-1/2 sm:translate-x-[-50%] flex-row justify-center items-center">
                                 <div className="flex flex-row items-center bg-white rounded-full px-2 shadow-xl shadow-neutral-200/30">
                                     <div className="visible opacity-100 sm:hidden sm:opacity-0 duration-300 mr-auto flex flex-row items-center">
-                                        <img src={process.env.AVATAR} alt="Avatar" className="w-8 h-8 rounded-full border-2" />
+                                        <img src={process.env.AVATAR} alt="Avatar" className="w-10 h-10 rounded-full border-2" />
                                         <div className="flex flex-col justify-center items-start mx-2">
                                             <p className="text-sm font-bold">
                                                 {process.env.NAME}
@@ -44,7 +44,7 @@ export function Header() {
                                     {profile?.permission && <NavItem title="写作" selected={location.startsWith("/writing")} herf="/writing" />}
                                     <NavItem title="朋友们" selected={location === "/friends"} herf="/friends" />
                                     <NavItem title="关于" selected={location === "/about"} herf="/about" />
-                                    <UserAvatar className="visible opacity-100 sm:hidden sm:opacity-0 duration-300 justify-center items-center w-12 h-12" profile={profile} />
+                                    <UserAvatar className="visible opacity-100 sm:hidden sm:opacity-0 duration-300 justify-center items-center h-12" profile={profile} />
                                 </div>
                             </div>
                             <UserAvatar className="ml-auto hidden opacity-0 sm:block sm:opacity-100 duration-300" profile={profile} />
@@ -71,14 +71,21 @@ function UserAvatar({ profile, className }: { className?: string, profile?: Prof
             <div className="relative">
                 <img src={profile.avatar} alt="Avatar" className="w-10 h-10 rounded-full border-2" />
                 <div className="z-50 absolute left-0 top-0 w-10 h-10 opacity-0 hover:opacity-100 duration-300">
-                    <Icon label="退出登录" name="ri-login-circle-line ri-xl" onClick={() => {
+                    <Icon label="退出登录" name="ri-logout-circle-line ri-xl" onClick={() => {
                         removeCookie("token")
                         window.location.reload()
                     }} hover={false} />
                 </div>
             </div>
         </> : <>
-            <Icon label="Github 登录" name="ri-login-circle-line ri-md" onClick={() => window.location.href = `${oauth_url}`} />
+            <button title="Github 登录" aria-label="Github 登录"
+                onClick={() => window.location.href = `${oauth_url}`}
+                className="flex rounded-full sm:rounded-xl border h-10 sm:h-auto px-2 py-2 bg-white items-center justify-center hover:bg-neutral-200">
+                <i className="ri-github-line ri-xl"></i>
+                <p className="text-sm ml-1 hidden sm:block">
+                    Github 登录
+                </p>
+            </button>
         </>}
     </div>)
 }
--- client/src/components/icon.tsx
+++ client/src/components/icon.tsx
@@ -1,6 +1,6 @@
 export function Icon({ name, label, className, onClick, hover = true }: { name: string, label: string, className?: string, onClick: () => any, hover?: boolean }) {
     return (
-        <button aria-label={label} onClick={onClick} className={`max-w-12 flex rounded-full border px-2 bg-white aspect-[1] items-center justify-center ${hover ? "hover:bg-neutral-200" : ""} ` + className}>
+        <button title={label} aria-label={label} onClick={onClick} className={`max-w-12 flex rounded-full border px-2 bg-white aspect-[1] items-center justify-center ${hover ? "hover:bg-neutral-200" : ""} ` + className}>
             <i className={name}></i>
         </button>
     )
@@ -8,7 +8,7 @@ export function Icon({ name, label, className, onClick, hover = true }: { name: 
 
 export function IconSmall({ name, label, className, onClick, hover = true }: { name: string, label: string, className?: string, onClick: () => any, hover?: boolean }) {
     return (
-        <button aria-label={label} onClick={onClick} className={`max-w-8 flex rounded-full border px-2 bg-white aspect-[1] items-center justify-center ${hover ? "hover:bg-neutral-200" : ""} ` + className}>
+        <button title={label} aria-label={label} onClick={onClick} className={`max-w-8 flex rounded-full border px-2 bg-white aspect-[1] items-center justify-center ${hover ? "hover:bg-neutral-200" : ""} ` + className}>
             <i className={name}></i>
         </button>
     )
