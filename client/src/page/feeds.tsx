9cefb019ba533ad942fca0ac039d34260b346302
Xeu <thankrain@qq.com>
2024-06-04 00:17:00 +0800

    fix: block all feeds in guest mode

--- client/src/page/feeds.tsx
+++ client/src/page/feeds.tsx
@@ -33,7 +33,7 @@ function Feeds() {
     }, [])
     const feed_filtered = feeds?.filter(
         ({ draft, listed }: { draft: number | undefined, listed: number | undefined }) =>
-            listState === 'draft' ? draft === 1 : listState === 'unlisted' ? listed === 0 : draft === 0 && listed === 1)
+            listState === 'draft' ? draft === 1 : listState === 'unlisted' ? listed === 0 : draft != 1 && listed != 0)
     return (
         <>
             <Waiting wait={feed_filtered}>
