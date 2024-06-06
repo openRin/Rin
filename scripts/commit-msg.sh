c6700396ff45c30c4fe8fd303ec6b6b8c362a675
Xeu <thankrain@qq.com>
2024-06-06 14:28:26 +0800

    chore: Add "check" script to package.json

---
+++ scripts/commit-msg.sh
@@ -0,0 +1,51 @@
+#!/bin/sh
+
+bun check
+if [ $? -ne 0 ]; then
+    echo "TypeScript compilation failed. Please fix the errors before committing."
+    exit 1
+fi
+
+#!/bin/sh
+
+# 获取 commit-msg 钩子传递的参数，即提交信息文件的路径
+COMMIT_MSG_FILE="$1"
+# 读取提交信息文件的内容
+COMMIT_MSG=$(cat $COMMIT_MSG_FILE)
+# 检查提交信息格式
+if ! echo "$COMMIT_MSG" | grep -E '^(feat|chore|fix|docs|ci|style|test|pref): ' > /dev/null; then
+    echo "ERROR: Commit message does not start with one of the following: feat|chore|fix|docs|ci|style|test|pref"
+    echo "Please ensure your commit message starts with one of these prefixes followed by a colon and a space."
+    exit 1
+fi
+
+exit 0
+
+
+
+
+
+#!/bin/sh
+#
+# An example hook script to check the commit log message.
+# Called by "git commit" with one argument, the name of the file
+# that has the commit message.  The hook should exit with non-zero
+# status after issuing an appropriate message if it wants to stop the
+# commit.  The hook is allowed to edit the commit message file.
+#
+# To enable this hook, rename this file to "commit-msg".
+
+# Uncomment the below to add a Signed-off-by line to the message.
+# Doing this in a hook is a bad idea in general, but the prepare-commit-msg
+# hook is more suited to it.
+#
+# SOB=$(git var GIT_AUTHOR_IDENT | sed -n 's/^\(.*>\).*$/Signed-off-by: \1/p')
+# grep -qs "^$SOB" "$1" || echo "$SOB" >> "$1"
+
+# This example catches duplicate Signed-off-by lines.
+
+test "" = "$(grep '^Signed-off-by: ' "$1" |
+         sort | uniq -c | sed -e '/^[   ]*1[    ]/d')" || {
+        echo >&2 Duplicate Signed-off-by lines.
+        exit 1
+}
