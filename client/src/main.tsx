8c2035dadca7a8c27f53821f6e28c440913f64d1
Xeu <thankrain@qq.com>
2024-06-07 21:01:27 +0800

    chore: Update page title in index.html and main.tsx
    Remove the hardcoded page title "Xeu" from index.html and add the dynamic page title using the siteName constant in main.tsx.

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,17 +1,23 @@
 import { treaty } from '@elysiajs/eden'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Helmet } from 'react-helmet'
 import 'remixicon/fonts/remixicon.css'
 import { App as Server } from 'rin-server/src/server'
 import App from './App'
 import './index.css'
+import { siteName } from './utils/constants'
 import { listenSystemMode } from './utils/darkModeUtils'
+
 export const endpoint = process.env.API_URL || 'http://localhost:3001'
 export const oauth_url = process.env.OAUTH_URL || (process.env.API_URL + '/user/github')
 export const client = treaty<Server>(endpoint)
 listenSystemMode()
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <Helmet>
+      <title>{siteName}</title>
+    </Helmet>
     <App />
   </React.StrictMode>
 )
