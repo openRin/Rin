d9561c1bde263cc223724d7a86baa7e8c5ce5d8f
Xeu <i@xeu.life>
2024-07-02 23:03:13 +0800

    fix: unable to jump to dest by index

--- client/src/components/markdown.tsx
+++ client/src/components/markdown.tsx
@@ -170,14 +170,14 @@ export function Markdown({ content }: { content: string }) {
         },
         h1({ children, ...props }) {
           return (
-            <h1 className="text-3xl font-bold mt-4" {...props}>
+            <h1 id={children?.toString()} className="text-3xl font-bold mt-4" {...props}>
               {children}
             </h1>
           );
         },
         h2({ children, ...props }) {
           return (
-            <h2 className="text-2xl font-bold mt-8" {...props}>
+            <h2 id={children?.toString()} className="text-2xl font-bold mt-8" {...props}>
               {children}
               <div className="h-[2px] border-b" />
             </h2>
@@ -185,28 +185,28 @@ export function Markdown({ content }: { content: string }) {
         },
         h3({ children, ...props }) {
           return (
-            <h3 className="text-xl font-bold mt-4" {...props}>
+            <h3 id={children?.toString()} className="text-xl font-bold mt-4" {...props}>
               {children}
             </h3>
           );
         },
         h4({ children, ...props }) {
           return (
-            <h4 className="text-lg font-bold mt-4" {...props}>
+            <h4 id={children?.toString()} className="text-lg font-bold mt-4" {...props}>
               {children}
             </h4>
           );
         },
         h5({ children, ...props }) {
           return (
-            <h5 className="text-base font-bold mt-4" {...props}>
+            <h5 id={children?.toString()} className="text-base font-bold mt-4" {...props}>
               {children}
             </h5>
           );
         },
         h6({ children, ...props }) {
           return (
-            <h6 className="text-sm font-bold mt-4" {...props}>
+            <h6 id={children?.toString()} className="text-sm font-bold mt-4" {...props}>
               {children}
             </h6>
           );
