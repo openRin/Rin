eba79471543568775f82ff2cd8a565933b8f31f9
Xeu <i@xeu.life>
2024-07-05 23:05:52 +0800

    feat: add useCache hook

--- client/src/components/input.tsx
+++ client/src/components/input.tsx
@@ -1,13 +1,9 @@
-import { Cache, Keys } from "../page/writing"
-
-export function Input({ value, setValue, className, placeholder, id, name }: { value: string, className?: string, placeholder: string, id?: number, name?: Keys, setValue: React.Dispatch<React.SetStateAction<string>> }) {
+export function Input({ value, setValue, className, placeholder }: { value: string, className?: string, placeholder: string, id?: number, setValue: (v: string) => void }) {
     return (<input
         placeholder={placeholder}
         value={value}
         onChange={(event) => {
             setValue(event.target.value)
-            if (name)
-                Cache.with(id).set(name, event.target.value)
         }}
         className={'bg-secondary w-full py-2 px-4 rounded-xl bg-w t-primary ' + className} />
     )
