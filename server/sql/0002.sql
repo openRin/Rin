03b2b78e40f8e5e497c19bacba69a7731d9191b2
Xeu <i@xeu.life>
2024-07-12 21:47:59 +0800

    fix: sql sepreator

--- server/sql/0002.sql
+++ server/sql/0002.sql
@@ -2,5 +2,7 @@ CREATE TABLE IF NOT EXISTS `info` (
 	`key` text NOT NULL,
 	`value` text NOT NULL
 );
+--> statement-breakpoint
 CREATE UNIQUE INDEX `info_key_unique` ON `info` (`key`);
+--> statement-breakpoint
 INSERT INTO `info` (`key`, `value`) VALUES ('migration_version', '2');
