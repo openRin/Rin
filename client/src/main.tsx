66e18eaa7a04ececac19bcf4f3e9bbef093e6e63
eric1197 <ccme66@gmail.com>
2024-12-06 13:16:16 +0800

    feat: add i18n(zh-tw) support (and rename 'zh' as 'zh-CN')

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -28,7 +28,6 @@ i18n
     // the translations
     // (tip move them in a JSON file and import them,
     // or even better, manage them via a UI: https://react.i18next.com/guides/multiple-translation-files#manage-your-translations-with-a-management-gui)
-    load: 'languageOnly',
     fallbackLng: "en",
     interpolation: {
       escapeValue: false // react already safes from xss => https://www.i18next.com/translation-function/interpolation#unescape
--- client/src/utils/timeago.ts
+++ client/src/utils/timeago.ts
@@ -1,7 +1,7 @@
 import { format } from "@astroimg/timeago";
-import i18n from "i18next"
+import i18n from "i18next";
 
 export function timeago(time: string | number | Date) {
-    const locale = i18n.language !== 'zh' ? 'en' : 'zh-CN'
+    const locale = i18n.language !== 'zh-CN' ? 'en' : 'zh-CN'
     return format(time, "DEFAULT", locale)
 }
