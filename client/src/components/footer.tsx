9bf0f3b96300e3573f78013d3d2d23474eac9827
358120997 <60354643+358120997@users.noreply.github.com>
2025-03-30 10:12:00 +0800

    footer.tsx

--- client/src/components/footer.tsx
+++ client/src/components/footer.tsx
@@ -59,7 +59,7 @@ function Footer() {
                             setDoubleClickTimes(doubleClickTimes + 1)
                         }
                     }}>
-                        © 2024 Powered by <a className='hover:underline' href="https://github.com/openRin/Rin" target="_blank">Rin</a>
+                        © 2025 Powered
                     </span>
                     {config.get<boolean>('rss') && <>
                         <Spliter />
