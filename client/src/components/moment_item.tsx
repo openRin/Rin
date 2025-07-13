7f062e8379d431635eb098627056227676330abe
Xeu <i@xeu.life>
2025-07-13 07:42:33 +0000

    style(moments): adjust moments style & remove font cdn

--- client/src/components/moment_item.tsx
+++ client/src/components/moment_item.tsx
@@ -29,24 +29,24 @@ export function MomentItem({
     const { createdAt, updatedAt } = moment;
     
     return (
-        <div className="p-4 rounded-lg border border-gray-200 dark:border-gray-800">
+        <div className="bg-w p-4 rounded-lg">
             <div className="flex justify-between">
                 <div className="flex items-center space-x-3">
                     <img 
                         src={moment.user.avatar} 
                         alt={moment.user.username} 
-                        className="w-10 h-10 rounded-full object-cover"
+                        className="w-8 h-8 rounded-full object-cover"
                     />
                     <div>
-                        <div className="font-medium text-black dark:text-white">
+                        <p className="t-primary">
                             {moment.user.username}
-                        </div>
-                        <p className="space-x-2"> 
-                            <span className="text-gray-400 text-sm" title={new Date(createdAt).toLocaleString()}> 
+                        </p>
+                        <p className="space-x-2 t-secondary text-sm"> 
+                            <span title={new Date(createdAt).toLocaleString()}> 
                                 {createdAt === updatedAt ? timeago(createdAt) : t('feed_card.published$time', { time: timeago(createdAt) })} 
                             </span> 
                             {createdAt !== updatedAt && 
-                                <span className="text-gray-400 text-sm" title={new Date(updatedAt).toLocaleString()}> 
+                                <span title={new Date(updatedAt).toLocaleString()}> 
                                     {t('feed_card.updated$time', { time: timeago(updatedAt) })} 
                                 </span> 
                             } 
@@ -54,21 +54,23 @@ export function MomentItem({
                     </div>
                 </div>
                 {canManage && (
-                    <div className="flex gap-2 pb-10">
-                        <button 
-                            aria-label={t("edit")} 
-                            onClick={() => onEdit(moment)}
-                            className="flex-1 flex flex-col items-end justify-center px-2 py bg-secondary bg-button rounded-full transition" 
-                        > 
-                            <i className="ri-edit-2-line dark:text-neutral-400" /> 
-                        </button> 
-                        <button 
-                            aria-label={t("delete.title")} 
-                            onClick={() => onDelete(moment.id)}
-                            className="flex-1 flex flex-col items-end justify-center px-2 py bg-secondary bg-button rounded-full transition" 
-                        > 
-                            <i className="ri-delete-bin-7-line text-red-500" /> 
-                        </button>
+                    <div>
+                        <div className="flex gap-2">
+                            <button
+                                aria-label={t("edit")}
+                                onClick={() => onEdit(moment)}
+                                className="flex-1 flex flex-col items-end justify-center px-2 py bg-secondary bg-button rounded-full transition"
+                            >
+                                <i className="ri-edit-2-line dark:text-neutral-400" />
+                            </button>
+                            <button
+                                aria-label={t("delete.title")}
+                                onClick={() => onDelete(moment.id)}
+                                className="flex-1 flex flex-col items-end justify-center px-2 py bg-secondary bg-button rounded-full transition"
+                            >
+                                <i className="ri-delete-bin-7-line text-red-500" />
+                            </button>
+                        </div>
                     </div>
                 )}
             </div>
