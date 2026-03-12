72ce3ae9cbf399726e6ba7d117477f986da62dc8
Xeu <i@xeu.life>
2026-03-12 17:20:13 +0000

    revert: restore fetch handler asset routing

--- server/src/runtime/fetch-handler.ts
+++ server/src/runtime/fetch-handler.ts
@@ -1,6 +1,7 @@
 import { getApp } from "./app-instance";
 
 const ROOT_FEED_PATTERN = /^\/(rss\.xml|atom\.xml|rss\.json|feed\.json|feed\.xml)$/;
+const APP_PUBLIC_ROUTE_PATTERN = /^\/(favicon|favicon\.ico)(?:\/|$)/;
 
 function isApiRequest(pathname: string) {
   return pathname.startsWith("/api/");
@@ -14,6 +15,10 @@ function rewriteApiRequest(request: Request) {
 
 function isRootFeedRequest(pathname: string) {
   return ROOT_FEED_PATTERN.test(pathname);
+}
+
+function isAppPublicRoute(pathname: string) {
+  return APP_PUBLIC_ROUTE_PATTERN.test(pathname);
 }
 
 function isStaticAssetRequest(pathname: string) {
@@ -64,13 +69,15 @@ export async function handleFetch(request: Request, env: Env): Promise<Response>
     return getApp().fetch(rewriteApiRequest(request), env);
   }
 
+  if (isAppPublicRoute(pathname)) {
+    return getApp().fetch(request, env);
+  }
+
   if (isStaticAssetRequest(pathname)) {
     const asset = await tryServeAsset(request, env);
     if (asset) {
       return asset;
     }
-
-    return getApp().fetch(request, env);
   }
 
   const indexResponse = await serveSpaEntry(request, env);
