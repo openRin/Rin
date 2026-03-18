1ac4c82f5425298ef40eeb3f2e50246bdd6ada1e
Nee <AntigoneSemioticsClub@users.noreply.github.com>
2026-03-18 18:53:23 +0800

    fix(ui): apply dynamic theme color to loading animation

--- packages/ui/src/loading.tsx
+++ packages/ui/src/loading.tsx
@@ -9,8 +9,8 @@ export function Waiting({
   children?: ReactNode;
 }) {
   return !wait ? (
-    <div className="w-full h-96 flex flex-col justify-center items-center mb-8 ani-show-fast">
-      <ReactLoading type="cylon" color="#FC466B" />
+    <div className="w-full h-96 flex flex-col justify-center items-center mb-8 ani-show-fast text-theme">
+      <ReactLoading type="cylon" color="currentColor" />
     </div>
   ) : (
     <>{children}</>
